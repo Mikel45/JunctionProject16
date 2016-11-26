76be75a64c469638bc441647a02c6609cb7992b0
mikel45 <ponomarev45@gmail.com>
2016-11-26 23:05:23 +0200

    Changes in scene Movie

---
+++ ProjectJuction2016/source/Assets/Scripts/MoovieScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoovieScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(PlayMovie());
+        }
+
+        protected IEnumerator PlayMovie()
+        {
+
+            Handheld.PlayFullScreenMovie("Movie.mp4", Color.blue, FullScreenMovieControlMode.Hidden);
+
+            yield return new WaitForSeconds(1.0f);
+
+            Application.LoadLevel("GameScene");
+        }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
